47518698d9a24963efc8776cc905e5d9af6785a1
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2023-11-08 02:27:42 -0500

    Updated After Testing

--- FolderIterator/searchText.java
+++ FolderIterator/searchText.java
@@ -2,57 +2,44 @@ package assignment03;
 
 import java.io.BufferedReader;
 import java.io.File;
-//import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 
-public class searchText {
+public class searchText2 {
 	public static void listFiles(File file) {
+		Scanner myObj = new Scanner(System.in);
+	    String text;
+	    // Enter text and press Enter
+	    System.out.println("Enter text: "); 
+	    text = myObj.nextLine().toUpperCase();
+	    myObj.close();
 		try (PrintWriter pw = new PrintWriter("Output3.txt")) {
-			Scanner myObj = new Scanner(System.in);
-		    String text;
-		    // Enter text and press Enter
-		    System.out.println("Enter text"); 
-		    text = myObj.nextLine(); 
 			System.out.println(file);
 			var iter = new FileCompositeIterator(file);
 			while(iter.hasNext()) {
 				File f = iter.next();
 				pw.println(f);
-				if (f.getName().toUpperCase().contains(text)){
-					System.out.println(f);
-				}
-				else {
-					if(f.getName().toUpperCase().contains(".TXT")
-					|| (f.getName().toUpperCase().contains(".HTM"))
-					|| (f.getName().toUpperCase().contains(".TEXT"))
-					|| (f.getName().toUpperCase().contains(".JAVA"))
-					|| (f.getName().toUpperCase().contains(".C"))
-					|| (f.getName().toUpperCase().contains(".CPP"))
-					|| (f.getName().toUpperCase().contains(".H"))
-					|| (f.getName().toUpperCase().contains(".HTML"))
-					|| (f.getName().toUpperCase().contains(".JS"))
-					|| (f.getName().toUpperCase().contains(".HS"))
-					|| (f.getName().toUpperCase().contains(".XML"))
-					|| (f.getName().toUpperCase().contains(".LOG"))){
-						BufferedReader reader = new BufferedReader(new FileReader(f));
-						String line;
-						while((line = reader.readLine()) != null) {
-							String[] temp = line.split("\\s+");
-							for(String s : temp)
-								if(s.toLowerCase().contains(text)) {
-									System.out.println(f);
-								}
-						}
-						reader.close();
-					}
+				if(f.getName().toUpperCase().contains(".TXT")
+				|| (f.getName().toUpperCase().contains(".HTM"))
+				|| (f.getName().toUpperCase().contains(".TEXT"))
+				|| (f.getName().toUpperCase().contains(".JAVA"))
+				|| (f.getName().toUpperCase().contains(".C"))
+				|| (f.getName().toUpperCase().contains(".CPP"))
+				|| (f.getName().toUpperCase().contains(".H"))
+				|| (f.getName().toUpperCase().contains(".HTML"))
+				|| (f.getName().toUpperCase().contains(".JS"))
+				|| (f.getName().toUpperCase().contains(".HS"))
+				|| (f.getName().toUpperCase().contains(".XML"))
+				|| (f.getName().toUpperCase().contains(".LOG"))){
+					if(checkWord(f, pw, text))
+						System.out.println(f);
 				}
 			}
-			myObj.close();
-		} catch (Exception e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
@@ -79,5 +66,31 @@ public class searchText {
 						System.out.println("DONE!");
 					}
 				});		
+	}
+
+	public static boolean checkWord(File f, PrintWriter pw, String text) {
+		boolean ct = false;
+		if (f.getName().toUpperCase().contains(text)){
+			ct = true;
+		}
+		else {
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(f));
+				String line;
+				while((line = reader.readLine()) != null) {
+					String[] temp = line.split("\\s+");
+					for(String s : temp)
+						if(s.toUpperCase().contains(text)) {
+							ct = true;
+							break;
+						}
+					}
+				reader.close();
+			} catch (Exception e) {
+				pw.println("PROBLEM WITH " + f);
+				e.printStackTrace(pw);
+			}	
+		}
+		return ct;
 	}
 }
